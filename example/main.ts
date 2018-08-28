53ef9bdf1bcd7ec574cbadc2f3b4fc6a5c12227d
maxmcd <max.t.mcdonnell@gmail.com>
2018-08-28 09:13:40 -0400

    Add timeouts to example script

--- example/main.ts
+++ example/main.ts
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+
+setTimeout(() => {
+  console.log("World");
+}, 10);
+
+console.log("Hello");
+
+const id = setTimeout(() => {
+  console.log("Not printed");
+}, 10000);
+
+clearTimeout(id);
