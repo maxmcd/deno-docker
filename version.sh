1f8b10bbd8c05862fe6af4a282100970bae60905
maxmcd <max.t.mcdonnell@gmail.com>
2019-01-01 12:28:12 -0800

    Add GitHub token, assume inconsistent GitHub version pulls are related to rate limiting

--- version.sh
+++ version.sh
@@ -1,7 +1,7 @@
 set -e
 set -o pipefail
 
-curl "https://api.github.com/repos/denoland/deno/releases/latest" |
+curl "https://api.github.com/repos/denoland/deno/releases/latest?access_token=$GITHUB_TOKEN" |
     grep '"tag_name":' |
     sed -E 's/.*"([^"]+)".*/\1/'
 
